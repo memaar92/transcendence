40de65824a319c53e56d978d42de3bb1d57881a4
dnebatz <danielnebatz@gmx.de>
2024-06-26 20:06:19 +0200

    added pillow and removed unsused

--- srcs/requirements/transpong/user/models.py
+++ srcs/requirements/transpong/user/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class User(models.Model):
     id = models.AutoField(primary_key=True)  # For an auto-incrementing int
     name = models.CharField(max_length=100)
@@ -21,4 +20,4 @@ class Games(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"Game {self.id}"
+        return f"Game {self.id} Home: {self.home_id} Visitor: {self.visitor_id}"
