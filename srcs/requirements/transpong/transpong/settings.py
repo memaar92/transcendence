7876d42b2f86afe4af6fbe21629100c7336a00b9
dnebatz <danielnebatz@gmx.de>
2024-06-26 12:46:40 +0200

    added user app and bootstrap

--- srcs/requirements/transpong/transpong/settings.py
+++ srcs/requirements/transpong/transpong/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user',
+    'django_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'transpong.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ srcs/requirements/transpong/user/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def user(request):
+    return render(request, 'user.html')
