7ed5b592bc3c7ee418e06c0c343982b46b16ef63
dnebatz <danielnebatz@gmx.de>
2024-06-26 13:41:54 +0200

    added first spa stuff

--- srcs/requirements/transpong/user/urls.py
+++ srcs/requirements/transpong/user/urls.py
@@ -2,5 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.user),
+    path('', views.layout, name='layout'),
+    
+    # Login URL
+    path('login/', views.login_view, name='login'),
+    
+    # Logout URL
+    path('logout/', views.logout_view, name='logout'),
+    
+    # Dynamically loaded content URLs
+    path('home/', views.home_content, name='home_content'),
+    path('about/', views.about_content, name='about_content')
 ]
