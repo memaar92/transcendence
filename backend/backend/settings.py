922b61b5d6cb9c8e7ae08f8dce0c8073214510bc
stevebalk <steve.balk@web.de>
2024-07-12 23:51:43 +0200

    Removed unused get ips from env function calls

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,16 +26,6 @@ SECRET_KEY = 'django-insecure-g=eom8w39!m$+1xegc@p!(6&uiqzdl$9i@$v5z!f$@m#2#_!7s
 # #TODO:  SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-# ALLOWED_HOSTS = [
-#     host for host in [
-#         "localhost",
-#         os.getenv("LOCAL_IP_ETH", ""),
-#         os.getenv("LOCAL_IP_WIFI", ""),
-#         os.getenv("IPv4", ""),
-#         os.getenv("IPv6", "")
-#     ] if host
-# ]
-
 #TODO: change to specific hosts
 ALLOWED_HOSTS = ["*"]
 
