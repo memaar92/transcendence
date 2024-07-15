47361e940eb28be6b29d6a6c758b8c4e081f4863
fbohling <fbohling@student.42wolfsburg.de>
2024-07-15 18:46:20 +0200

    working token auth in front end

--- backend/live_chat/consumers.py
+++ backend/live_chat/consumers.py
@@ -1,191 +1,194 @@
-import json
-from django.conf import settings
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
 from django.contrib.auth import get_user_model
 from .models import Chat, Message
 from django.utils import timezone
+from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
+from rest_framework_simplejwt.tokens import UntypedToken
+import json
 
 class ChatConsumer(AsyncWebsocketConsumer):
     user_id_to_channel_name = {}
-    
+
     async def connect(self):
-        if self.scope["user"].is_authenticated:
-            self.user_id = str(self.scope["user"].id)
-        else:
-            self.user_id = "anonymous_" + self.channel_name
-        ChatConsumer.user_id_to_channel_name[self.user_id] = self.channel_name
-        self.friends = []
-        await self.accept()
-        print("Connected to chat")
-        print(self.user_id)
-        print(self.channel_name)
-        await self.broadcast_user_list()
+        # Extract the token from the query string
+        token = self.scope['query_string'].decode().split('=')[1]
+
+        if token:
+            user_id = await self.get_user_id_from_jwt(token)
+            if user_id:
+                user = await self.get_user(user_id)
+                if user:
+                    self.scope['user'] = user
+                    self.user_id = str(user.id)
+                    ChatConsumer.user_id_to_channel_name[self.user_id] = self.channel_name
+                    await self.accept()
+                    print("Connected to chat")
+                    return
+
+        # Close the connection if no valid token is provided
+        await self.close()
+
+    @database_sync_to_async
+    def get_user_id_from_jwt(self, token):
+        try:
+            
+            decoded_data = UntypedToken(token)
+            user_id = decoded_data['user_id']
+            return user_id
+        except (InvalidToken, TokenError) as e:
+            print(f"Error decoding token: {e}")
+            return None
+
+    @database_sync_to_async
+    def get_user(self, user_id):
+        User = get_user_model()
+        try:
+            return User.objects.get(id=user_id)
+        except User.DoesNotExist:
+            return None
 
     async def disconnect(self, close_code):
         if self.user_id in ChatConsumer.user_id_to_channel_name:
             del ChatConsumer.user_id_to_channel_name[self.user_id]
-        print("Disconnected from chat")
-        await self.close()
-        await self.broadcast_user_list()
 
-    async def broadcast_user_list(self):
-        users = await self.get_user_list()
-        for user_id, channel_name in ChatConsumer.user_id_to_channel_name.items():
-            modified_users = [
-                f"{user}(you)" if str(user) == user_id else user
-                for user in users
-            ]
-            user_list_message = json.dumps({
-                'type': 'user_list',
-                'users': modified_users
-            })
-            await self.channel_layer.send(channel_name, {
-                'type': 'send_websocket_message',
-                'text': user_list_message
-            })
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        message_type = data.get('type', None)
 
-    async def send_websocket_message(self, event):
-        await self.send(text_data=event['text'])
+        if message_type == 'chat_request':
+            await self.chat_request(data)
+        elif message_type == 'chat_message':
+            await self.chat_message(data)
+        elif message_type == 'chat_request_accepted':
+            await self.chat_request_accepted(data)
+        elif message_type == 'chat_request_denied':
+            await self.chat_request_denied(data)
+        elif message_type == 'add_friend':
+            await self.add_friend(data)
+        elif message_type == 'chat_history':
+            await self.chat_history(data)
 
-    async def send_message_to_user(self, receiver_id, message, message_type):
-        receiver_id = str(receiver_id)
-        channel_name = ChatConsumer.user_id_to_channel_name.get(receiver_id)
-        print(f"USER {self.user_id} SENT MESSAGE TO {receiver_id}: {message} ({message_type})")
-        if channel_name:
-            await self.channel_layer.send(channel_name, {
-                "type": "send_websocket_message",
-                "text": json.dumps({
-                    "type": message_type,
-                    "message": message,
-                    "sender_id": self.user_id
-                }),
-            })
+    async def chat_request(self, data):
+        receiver_id = data['receiver_id']
+        sender_id = data['sender_id']
+        if receiver_id in ChatConsumer.user_id_to_channel_name:
+            receiver_channel_name = ChatConsumer.user_id_to_channel_name[receiver_id]
+            await self.channel_layer.send(
+                receiver_channel_name,
+                {
+                    'type': 'chat_request_notification',
+                    'sender_id': sender_id
+                }
+            )
 
-    async def chat_request(self, event):
-        sender_id = self.user_id
-        receiver_id = event['receiver_id']
-        await self.send_message_to_user(receiver_id, f'You have a new chat request from {sender_id}', 'chat_request_notification')
+    async def chat_message(self, data):
+        receiver_id = data['receiver_id']
+        sender_id = data['sender_id']
+        message = data['message']
+        if receiver_id in ChatConsumer.user_id_to_channel_name:
+            receiver_channel_name = ChatConsumer.user_id_to_channel_name[receiver_id]
+            await self.channel_layer.send(
+                receiver_channel_name,
+                {
+                    'type': 'chat_message',
+                    'message': message,
+                    'sender_id': sender_id
+                }
+            )
+        await self.save_message(sender_id, receiver_id, message)
 
-    async def chat_request_notification(self, event):
-        sender_id = event['sender_id']
-        message = event['message']
+    async def chat_request_accepted(self, data):
+        sender_id = data['sender_id']
+        receiver_id = data['receiver_id']
+        if sender_id in ChatConsumer.user_id_to_channel_name:
+            sender_channel_name = ChatConsumer.user_id_to_channel_name[sender_id]
+            await self.channel_layer.send(
+                sender_channel_name,
+                {
+                    'type': 'chat_request_accepted',
+                    'receiver_id': receiver_id
+                }
+            )
+
+    async def chat_request_denied(self, data):
+        sender_id = data['sender_id']
+        receiver_id = data['receiver_id']
+        if sender_id in ChatConsumer.user_id_to_channel_name:
+            sender_channel_name = ChatConsumer.user_id_to_channel_name[sender_id]
+            await self.channel_layer.send(
+                sender_channel_name,
+                {
+                    'type': 'chat_request_denied',
+                    'receiver_id': receiver_id
+                }
+            )
+
+    async def add_friend(self, data):
+        receiver_id = data['receiver_id']
+        sender_id = data['sender_id']
+        if receiver_id in ChatConsumer.user_id_to_channel_name:
+            receiver_channel_name = ChatConsumer.user_id_to_channel_name[receiver_id]
+            await self.channel_layer.send(
+                receiver_channel_name,
+                {
+                    'type': 'add_friend',
+                    'receiver_id': receiver_id,
+                    'sender_id': sender_id
+                }
+            )
+
+    async def chat_history(self, data):
+        sender_id = data['sender_id']
+        receiver_id = data['receiver_id']
+        messages = await self.get_chat_history(sender_id, receiver_id)
         await self.send(text_data=json.dumps({
-            'type': 'chat_request_notification',
-            'message': message,
-            'sender_id': sender_id
+            'type': 'chat_history',
+            'messages': messages
         }))
 
-    async def chat_request_denied(self, event):
-        sender_id = event['sender_id']
-        receiver_id = event['receiver_id']
+    @database_sync_to_async
+    def get_chat_history(self, sender_id, receiver_id):
+        messages = Message.objects.filter(
+            (Q(sender_id=sender_id) & Q(receiver_id=receiver_id)) |
+            (Q(sender_id=receiver_id) & Q(receiver_id=sender_id))
+        ).order_by('timestamp')
+        return [{'sender_id': msg.sender_id, 'content': msg.content, 'timestamp': msg.timestamp} for msg in messages]
+
+    @database_sync_to_async
+    def save_message(self, sender_id, receiver_id, message):
+        sender = get_user_model().objects.get(id=sender_id)
+        receiver = get_user_model().objects.get(id=receiver_id)
+        Message.objects.create(sender=sender, receiver=receiver, content=message, timestamp=timezone.now())
+
+    async def chat_request_notification(self, event):
         await self.send(text_data=json.dumps({
-            'type': 'chat_request_denied',
-            'sender_id': sender_id,
-            'receiver_id': receiver_id
+            'type': 'chat_request_notification',
+            'sender_id': event['sender_id']
+        }))
+
+    async def chat_message(self, event):
+        await self.send(text_data=json.dumps({
+            'type': 'chat_message',
+            'message': event['message'],
+            'sender_id': event['sender_id']
         }))
 
     async def chat_request_accepted(self, event):
-        sender_id = event['sender_id']
-        receiver_id = event['receiver_id']
-        self.friends.append(sender_id)
-        await self.send_message_to_user(sender_id, "You are now friends with " + receiver_id, 'add_friend')
-    
+        await self.send(text_data=json.dumps({
+            'type': 'chat_request_accepted',
+            'receiver_id': event['receiver_id']
+        }))
+
+    async def chat_request_denied(self, event):
+        await self.send(text_data=json.dumps({
+            'type': 'chat_request_denied',
+            'receiver_id': event['receiver_id']
+        }))
+
     async def add_friend(self, event):
-        receiver_id = event['receiver_id']
-        self.friends.append(receiver_id)
-
-    async def receive(self, text_data=None, bytes_data=None):
-        if text_data:
-            text_data_json = json.loads(text_data)
-            message_type = text_data_json.get('type')
-            receiver_id = text_data_json.get('receiver_id')
-            content = text_data_json.get('message')
-            sender_id = text_data_json.get('sender_id')
-
-            print(message_type)
-            match message_type:
-                case 'chat_request':
-                    await self.chat_request(text_data_json)
-                case 'chat_request_notification':
-                    await self.chat_request_notification(text_data_json)
-                case 'chat_request_accepted':
-                    await self.chat_request_accepted(text_data_json)
-                case 'chat_request_denied':
-                    await self.chat_request_denied(text_data_json)
-                case 'add_friend':
-                    await self.add_friend(text_data_json)
-                case 'chat_message':
-                    if receiver_id in self.friends:
-                        await self.send_message_to_user(receiver_id, content, 'chat_message')
-                        # await self.save_message(await self.get_or_create_chat(sender_id, receiver_id), content, sender_id, receiver_id)
-                    else:
-                        await self.send_message_to_user(sender_id, "You are not friends with " + receiver_id, 'chat_message')
-                case 'request_user_list':
-                    await self.send_user_list()
-                case 'chat_history':
-                    await self.send_chat_history(sender_id, receiver_id)
-                case _:
-                    print('Unknown message type')
-
-    async def send_user_list(self):
-        users = await self.get_user_list()
         await self.send(text_data=json.dumps({
-            'type': 'user_list',
-            'users': users
+            'type': 'add_friend',
+            'receiver_id': event['receiver_id'],
+            'sender_id': event['sender_id']
         }))
-
-    async def send_chat_history(self, sender_id, receiver_id):
-        chat = await self.get_or_create_chat(sender_id, receiver_id)
-        messages = await self.get_chat_messages(chat)
-        await self.send(text_data=json.dumps({
-            'type': 'chat_history',
-            'messages': [
-                {
-                    'sender_id': message.sender.id,
-                    'receiver_id': message.receiver.id,
-                    'content': message.content,
-                    'timestamp': message.timestamp
-                }
-                for message in messages
-            ]
-        }))
-
-    @database_sync_to_async
-    def get_user_list(self):
-        connected_user_ids = list(ChatConsumer.user_id_to_channel_name.keys())
-        connected_user_ids_str = [str(user_id) for user_id in connected_user_ids]
-        return connected_user_ids_str
-
-    @database_sync_to_async
-    def get_or_create_chat(self, sender_id, receiver_id):
-        User = get_user_model()
-        try:
-            sender = User.objects.get(id=sender_id)
-            receiver = User.objects.get(id=receiver_id)
-        except User.DoesNotExist:
-            return None
-
-        chats = Chat.objects.filter(participants=sender).filter(participants=receiver)
-        chats = [chat for chat in chats if chat.participants.count() == 2]
-
-        if chats:
-            return chats[0]
-        else:
-            chat = Chat.objects.create()
-            chat.participants.add(sender, receiver)
-            return chat
-
-    @database_sync_to_async
-    def save_message(self, chat, content, sender_id, receiver_id):
-        User = get_user_model()  # Again, use get_user_model here
-        try:
-            sender = User.objects.get(id=sender_id)
-            receiver = User.objects.get(id=receiver_id)
-        except User.DoesNotExist:
-            return None
-        return Message.objects.create(chat=chat, content=content, sender=sender, receiver=receiver, timestamp=timezone.now())
-
-    @database_sync_to_async
-    def get_chat_messages(self, chat):
-        return Message.objects.filter(chat=chat).order_by('timestamp')
