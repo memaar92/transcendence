4fffbf75fa8160e575353bce3d6f8811c65a0a16
wrottger <wrottger@student.42wolfsburg.de>
2024-07-16 15:06:14 +0200

    initial commit

---
+++ frontend/js/app.js
@@ -0,0 +1,17 @@
+import Router from './router.js';
+
+const routes = [
+  { path: '/', templateUrl: '/routes/home.html' },
+  { path: '/home', templateUrl: '/routes/home.html' },
+  { path: '/email_verification', templateUrl: '/routes/email_verification.html' },
+  { path: '/login', templateUrl: '/routes/login.html' },
+  { path: '/register', templateUrl: '/routes/register.html' },
+  { path: '/main_menu', templateUrl: '/routes/main_menu.html' },
+  { path: '/404', templateUrl: '/routes/404.html' },
+];
+
+const router = new Router(routes);
+
+document.addEventListener('DOMContentLoaded', () => {
+  router.init(document.getElementById('app'));
+});
