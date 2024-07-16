61a19ae3b582e9c54ac3cfeaadb5ea49d5301bc1
memaar92 <marius.messerschmied@gmail.com>
2024-07-16 18:24:01 +0200

    clean up and add comments to 42 auth mechanism

--- backend/auth42/urls.py
+++ backend/auth42/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('init/', views.index, name='42auth_init'),
-	path('', views.index2, name='42auth'),
-	path('test/', views.test, name='42auth_test'),
+	path('', views.auth42, name='auth_42'),
+	path('redirect/', views.redirect42, name='redirect42'),
 ]
--- backend/auth42/views.py
+++ backend/auth42/views.py
@@ -1,13 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from rest_framework import generics
+from rest_framework import generics, status
 from django.views import generic
 from rest_framework.response import Response
-from rest_framework import status
 from rest_framework.views import APIView
-from django.shortcuts import redirect
 import requests
 
+# create more elegant way to get secrets as function is used in multiple places (e.g. settings.py)
 def get_secret(secret_name):
     try:
         with open(f'/run/secrets/{secret_name}') as secret_file:
@@ -15,27 +14,29 @@ def get_secret(secret_name):
     except IOError as e:
             raise Exception(f'Critical error reading secret {secret_name}: {e}')
 
-def index(request):
-    return redirect('https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-a2e745a099029f6f35be3df957c45817c6692892087079ba929ccc3610b635c3&redirect_uri=https%3A%2F%2Flocalhost%2F42auth&response_type=code')
+def redirect42(request):
+    target_url = 'https://api.intra.42.fr/oauth/authorize?client_id=' + get_secret('oauth_client_id') + '&redirect_uri=https%3A%2F%2Flocalhost%2F42auth&response_type=code'
+    return redirect(target_url)
 
-def index2(request):
-    code = request.GET.get('code')
-    response = requests.post('https://api.intra.42.fr/oauth/token', data={
+def auth42(request):
+    if (code := request.GET.get('code') is None):
+        return HttpResponseBadRequest()
+    oauth_response = requests.post('https://api.intra.42.fr/oauth/token', data={
         'code': code,
         'grant_type': 'authorization_code',
         'client_id': get_secret('oauth_client_id'),
         'client_secret': get_secret('oauth_secret'),
         'redirect_uri': 'https://localhost/42auth'
     }).json()
-    print("oauth_response: ", response)
+    #error handling for oauth response
     user_info = requests.get('https://api.intra.42.fr/v2/me', headers={
-        'Authorization': 'Bearer ' + response['access_token']
+        'Authorization': 'Bearer ' + oauth_response['access_token']
     }).json()
     print("user info: ", user_info)
-    return HttpResponse("Hello, world. You're at the 42 auth test2.") # create access token and return it
-
-def test(request):
-    return HttpResponse("Hello, world. You're at the 42 auth test.")
+    #check if user is already in database (based on 42_id)
+    #if already in database, no need to register
+    #else: create new database entry (can I call a function from usermanagement.views here?)
+    return HttpResponse("Hello, world. You're at the 42 auth test2.") # create JWT access token and return it with JSON response (frontend as recipient) Should be same as with user registration
 
 
 #class AuthWith42(APIView):
