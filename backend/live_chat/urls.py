d976d9c1a7f17609b15463b0efca502c571a7f00
fbohling <fbohling@student.42wolfsburg.de>
2024-07-18 18:33:52 +0200

    attempting beautiful html

--- backend/live_chat/urls.py
+++ backend/live_chat/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.live_chat, name='live_chat'),
+    path('index.html', views.index, name='index'),
 ]
--- backend/live_chat/views.py
+++ backend/live_chat/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def live_chat(request):
     return render(request, 'live_chat.html')
+
+def index(request):
+    return render(request, 'index.html')
