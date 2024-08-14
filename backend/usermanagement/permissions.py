0bc35d1bea59866f233ace540a6fc278ba2c0934
dnebatz <dnebatz@student.42wolfsburg.de>
2024-08-14 18:29:35 +0200

    replaced

--- backend/usermanagement/permissions.py
+++ backend/usermanagement/permissions.py
@@ -6,26 +6,26 @@ import jwt
 from django.conf import settings
 
 class IsSelf(permissions.BasePermission):
-	def has_object_permission(self, request, view, obj):
-		return obj == request.user
+    def has_object_permission(self, request, view, obj):
+        return obj == request.user
 
 
 class Check2FA(permissions.BasePermission):
-	def has_permission(self, request, view):
-		auth_header = request.headers.get('Authorization')
-		if auth_header:
-			try:
-				# Extract the token part from the header
-				token = auth_header.split(' ')[1]
-				# Decode the token
-				payload = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
-				# Check if '2fa' in payload is None or True
-				print("Is2fa")
-				print(payload.get('2fa'))
-				if payload.get('2fa') in [0, 2]:
-					return True
-			except jwt.ExpiredSignatureError:
-				return False
-			except jwt.InvalidTokenError:
-				return False
-		raise PermissionDenied({'message': '2FA verification needed.'})
+    def has_permission(self, request, view):
+        auth_header = request.headers.get('Authorization')
+        if auth_header:
+            try:
+                # Extract the token part from the header
+                token = auth_header.split(' ')[1]
+                # Decode the token
+                payload = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
+                # Check if '2fa' in payload is None or True
+                print("Is2fa")
+                print(payload.get('2fa'))
+                if payload.get('2fa') in [0, 2]:
+                    return True
+            except jwt.ExpiredSignatureError:
+                return False
+            except jwt.InvalidTokenError:
+                return False
+        raise PermissionDenied({'message': '2FA verification needed.'})
