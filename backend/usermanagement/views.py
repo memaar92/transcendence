7d38499e61eb9ef9c779004888779f4543a80c76
memaar92 <marius.messerschmied@gmail.com>
2024-08-14 13:25:09 +0200

    fix bug in middleware

--- backend/usermanagement/views.py
+++ backend/usermanagement/views.py
@@ -29,11 +29,11 @@ import base64
 
 MAX_OTP_ATTEMPTS = 5
 OTP_LOCK_TIME = 300
+OTP_LIFETIME = 300
 
 
 #TO DO: add a cron job that regularly deletes users that have not verified their email within a certain time frame
 #TO DO: add a cron job that regularly deletes expired tokens from the blacklist
-#TO DO: log out endpoint
 
 
 #everywhere where access token is sent, if expired --> 401 / detail: token expired
@@ -228,6 +228,8 @@ class CustomTokenRefreshView(TokenRefreshView, CookieCreationMixin):
 	)
 
 	def post(self, request, *args, **kwargs):
+		print("request.META: ", request.META)
+		print("request.data: ", request.data)
 		response = super().post(request, *args, **kwargs)
 		self.createCookies(response)
 		response.data = {'detail': 'Access and refresh tokens successfully created'}
@@ -333,7 +335,7 @@ class GenerateOTPView(APIView):
 
 		otp = random.randint(100000, 999999)
 		otp_cache_key = f'{user_profile.id}_otp'
-		cache.set(otp_cache_key, otp, timeout=300)
+		cache.set(otp_cache_key, otp, timeout=OTP_LIFETIME)
 		cache.incr(otp_attempts_key)
 
 		#send email with otp // for testing this is disabled and the otp is printed in the console
