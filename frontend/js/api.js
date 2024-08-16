64ec2d0f65b47da6c68f8fda79c18d79e9497c73
wrottger <wrottger@student.42wolfsburg.de>
2024-08-16 17:50:14 +0200

    automatically request refresh token on GET request

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,8 +1,27 @@
+import { router } from "./app.js";
 const API_BASE_URL = "api";
 
 export const api = {
   get: async (endpoint) => {
     const response = await fetch(`${API_BASE_URL}${endpoint}`);
+    if (response.status == 401)
+    {
+      const json = await response.json()
+      if (json["code"] == "token_not_valid")
+      {
+        const response = await fetch(`${API_BASE_URL}/token/refresh/`, {
+          method : "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: "{\"refresh\" : \"hello\"}"
+        });
+        const json = await response.json()
+        if (json["code"] == "token_not_valid")
+          router.navigate("/home");
+        return await fetch(`${API_BASE_URL}${endpoint}`);
+      }
+    }
     return response;
   },
 
