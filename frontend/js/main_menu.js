889121739b8df05c1e39e0f86901e04fad06b669
wrottger <wrottger@student.42wolfsburg.de>
2024-09-04 17:36:46 +0200

    add profile button

--- frontend/js/main_menu.js
+++ frontend/js/main_menu.js
@@ -2,10 +2,15 @@ import { api } from "./api.js";
 import { router } from "./app.js";
 
 const result = await api.get("/profile/");
-const profile_info = await result.json()
+const profile_info = await result.json();
 
 const photo = document.getElementById("profile-photo");
 photo.src = profile_info["profile_picture"];
 
 const name = document.getElementById("username");
 name.innerHTML = profile_info["displayname"];
+
+document.getElementById("profile").addEventListener("click", async (e) => {
+    e.preventDefault();
+    router.navigate("/profile");
+});
