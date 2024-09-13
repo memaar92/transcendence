f8c3b75bbdb4f3e68d60050605eeb3f43bf9f756
wrottger <wrottger@student.42wolfsburg.de>
2024-09-13 15:42:22 +0200

    add stats page

--- frontend/js/stats.js
+++ frontend/js/stats.js
@@ -3,50 +3,69 @@ import { router } from "./app.js";
 
 const result = await api.get("/games/");
 const games = await result.json();
-const profile_info = await result.json()
 
-const photo = document.getElementById("profile-photo");
-photo.src = profile_info["profile_picture"];
-
-document.getElementById('displayname').value = profile_info["displayname"];
-
-const email = document.getElementById("email");
-email.value = profile_info["email"];
+console.log(games);
 
 document.getElementById("back").addEventListener("click", async (e) => {
   console.log("going back");
   history.back();
 });
 
-function tableCreate() {
-    const content = document.getElementById("content"),
-          tbl = document.createElement('table');
-    tbl.style.width = '100px';
-    tbl.style.border = '1px solid white';
-    console.log("MAKING TABLE")
-    console.log(content)
-  
-    for (let i = 0; i < 3; i++) {
-      const tr = tbl.insertRow();
-      for (let j = 0; j < 2; j++) {
-        if (i === 2 && j === 1) {
-          break;
-        } else {
-          const td = tr.insertCell();
-          td.appendChild(document.createTextNode(`Cell I${i}/J${j}`));
-          td.style.border = '1px solid white';
-          if (i === 1 && j === 1) {
-            td.setAttribute('rowSpan', '2');
-          }
-        }
-      }
-    }
-    content.appendChild(tbl);
+async function tableCreate() {
+  const table = document.getElementById("matchDataBody");
+  const myId = await getMyId();
+
+  if (games.length == 0) {
+    let row = table.insertRow();
+    let cell = row.insertCell();
+    cell.textContent = "No games Found";
+    cell.colSpan = 5;
   }
   
-tableCreate();
+  document.getElementById("games").textContent = games.length
+  let wins = 0;
+  let losses = 0;
+
+  for (let i = 0; i < games.length; i++) {
+    let row = table.insertRow();
+    row.insertCell(0).textContent = i + 1;
+    row.insertCell(1).textContent = await getUsername(myId);
+    if (games[i]["home_id"] == myId) {
+      row.insertCell(2).textContent = await getUsername(games[i]["visitor_id"]);
+      row.insertCell(
+        3
+      ).textContent = `${games[i]["home_score"]} - ${games[i]["visitor_score"]}`;
+      if (games[i]["home_score"] > games[i]["visitor_score"])
+        wins++;
+      else if (games[i]["home_score"] < games[i]["visitor_score"])
+        losses++;
+    } else {
+      row.insertCell(2).textContent = await getUsername(games[i]["home_id"]);
+      row.insertCell(
+        3
+      ).textContent = `${games[i]["visitor_score"]} - ${games[i]["home_score"]}`;
+      if (games[i]["visitor_score"] > games[i]["home_score"])
+        wins++;
+      else if (games[i]["visitor_score"] < games[i]["home_score"])
+        losses++;
+    }
+    let date = new Date(games[i]["created_at"]);
+    row.insertCell(4).textContent = date.toLocaleString("en-US");
+  }
+  document.getElementById("wins").textContent = wins;
+  document.getElementById("losses").textContent = losses;
+}
+
+await tableCreate();
 
 async function getUsername(id) {
-    const result = await api.get(`/users/${id}`);
-    const games = await result.json();
+  const result = await api.get(`/users/${id}/`);
+  const info = await result.json();
+  return info["displayname"];
+}
+
+async function getMyId() {
+  const result = await api.get(`/profile/`);
+  const info = await result.json();
+  return info["id"];
 }
