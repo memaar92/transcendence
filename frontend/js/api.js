0f8fdf04d0664fb8f459fd2fc954393b9ed760c7
wrottger <wrottger@student.42wolfsburg.de>
2024-09-30 16:54:39 +0200

    remove unused getCookie function

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -2,21 +2,6 @@ import { router } from "./app.js";
 const API_BASE_URL = "https://localhost/api";
 
 
-function getCookie(name) {
-    let cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        const cookies = document.cookie.split(';');
-        for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i].trim();
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
-}
-
 export const api = {
   get: async (endpoint) => {
     const response = await fetch(`${API_BASE_URL}${endpoint}`);
