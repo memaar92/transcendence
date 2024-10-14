65bb80b4779f983b214da8f2535807e476b645ee
wrottger <wrottger@student.42wolfsburg.de>
2024-10-14 10:25:26 +0200

    add error when trying to join queue in tournament

--- frontend/js/queue.js
+++ frontend/js/queue.js
@@ -1,15 +1,15 @@
-import { hubSocket } from "./app.js";
+import { hubSocket, showAlert } from "./app.js";
 import { router } from "./app.js";
 
-
 function game_start(message) {
-    if (message.type == "remote_match_ready")
-    {
-        window.localStorage.setItem("game_id", message.match_id)
-        router.navigate("/game")
-    }
+  if (message.type == "remote_match_ready") {
+    window.localStorage.setItem("game_id", message.match_id);
+    router.navigate("/game");
+  } else if (!message.queue_registered && message.message) {
+    showAlert(message.message);
+    router.navigate("/main_menu");
+  }
 }
 
 hubSocket.registerCallback(game_start);
-
-hubSocket.send({type: "queue_register"});
+hubSocket.send({ type: "queue_register" });
