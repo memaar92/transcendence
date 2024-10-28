ef1d0bb880997a661ecbefcfff4ba0705105dab1
dnebatz <dnebatz@student.42wolfsburg.de>
2024-10-28 17:15:42 +0100

    added en and decryption for totp secret

--- backend/utils/utils.py
+++ backend/utils/utils.py
@@ -1,13 +1,17 @@
 import uuid
 import random
-import os
+from cryptography.fernet import Fernet
 from django.apps import apps
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 from rest_framework_simplejwt.tokens import RefreshToken
+from django.conf import settings
 
 MAX_LENGTH_DISPLAYNAME = 20
+
+# Generate a Fernet key
+crypto_key = Fernet(settings.SECRET_KEY)
 
 def random_filename(instance, filename):
     ext = filename.split('.')[-1]
@@ -45,4 +49,15 @@ def send_otp_email(recipient, otp):
     to = recipient
 
     send_mail(subject, plain_message, from_email, [to], html_message=html_message, fail_silently=False)
-    
+
+def encrypt_totp_secret(totp_secret):
+    if isinstance(totp_secret, str):
+        totp_secret = totp_secret.encode('utf-8')
+    encrypted_secret = crypto_key.encrypt(totp_secret)
+    return encrypted_secret.decode('utf-8')
+
+def decrypt_totp_secret(encrypted_secret):
+    if isinstance(encrypted_secret, str):
+        encrypted_secret = encrypted_secret.encode('utf-8')
+    decrypted_secret = crypto_key.decrypt(encrypted_secret)
+    return decrypted_secret.decode('utf-8')
