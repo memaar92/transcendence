fcb8f1454f4b375c6faecfc847a063df4c256632
stevebalk <steve.balk@web.de>
2024-10-29 08:54:44 +0100

    Remove and rename files

--- backend/pong/views.py
+++ backend/pong/views.py
@@ -1,8 +1,3 @@
 from django.shortcuts import render
 
 # Create your views here.
-def pong(request):
-    return render(request, 'pong.html')
-
-def tournament(request):
-    return render(request, 'tournament.html')
